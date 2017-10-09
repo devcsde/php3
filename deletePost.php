11b25cb069f9399407bc9522d562145e21663be4
devcsde <c.scheidler@gmx.net>
2017-10-09 15:25:00 +0200

    refactor

--- deletePost.php
+++ deletePost.php
@@ -33,7 +33,7 @@ if(isset($_POST["Submit"])){
     <script src="js/bootstrap.min.js"></script>
 </head>
 <body>
-  <div class="cont1"></div>
+<div class="cont1"></div>
   <nav class="navbar navbar-inverse" role="navigation" style="border-radius:0px;">
       <div class="container">
           <div class="navbar-header myNav">
@@ -63,91 +63,90 @@ if(isset($_POST["Submit"])){
           </div>
       </div>
   </nav>
-  <div class="cont2"></div>
+<div class="cont2"></div>
 <div class="container-fluid">
 
-<div class="row">
-    <div class="col-sm-2">
-          &nbsp;
-        <ul id="sidemenu" class="nav nav-pills nav-stacked">
-            <li><a href="dashboard.php">
-                <span class="glyphicon glyphicon-th"></span>
-                &nbsp;Dashboard</a>
-            </li>
-            <li  class="active"><a href="addNewPost.php">
-                <span class="glyphicon glyphicon-list-alt"></span>
-                &nbsp;Neuer Artikel</a>
-            </li>
-            <li><a href="categories.php">
-                <span class="glyphicon glyphicon-tags"></span>
-                &nbsp;Kategorien</a>
-            </li>
-            <li><a href="#">
-                <span class="glyphicon glyphicon-user"></span>
-                &nbsp;Manage Admins</a>
-            </li>
-            <li><a href="#">
-                <span class="glyphicon glyphicon-comment"></span>
-                &nbsp;Kommentare</a>
-            </li>
-            <li><a href="#">
-                <span class="glyphicon glyphicon-equalizer"></span>
-                &nbsp;Live Blog</a>
-            </li>
-            <li><a href="#">
-                <span class="glyphicon glyphicon-log-out"></span>
-                &nbsp;Logout</a>
-            </li>
-        </ul>
-    </div> <!-- Sidebar End  -->
-    <div class="col-sm-10">
-	      <div>
-	          <?php
-	          echo message();
-	          echo okMessage();
-	          ?>
-	      </div>
-        <h1>Artikel löschen</h1>
-        <div>
-						<?php
-						$queryId = $_GET["delete"];
-						$connection;
-						$query = "SELECT * FROM admin_panel WHERE id='$queryId'";
-						$execute = mysqli_query($connection, $query);
-						while($dataRows = mysqli_fetch_array($execute)){
-							$title = $dataRows["title"];
-	            $category = $dataRows["category"];
-	            $image = $dataRows["image"];
-	            $post = $dataRows["post"];
-						}
-						?>
-            <form action="deletePost.php?delete=<?php echo $queryId; ?>" method="post" enctype="multipart/form-data">
-                <fieldset>
-                    <div class="form-group">
-                        <label for="title"><span class="fieldInfo">Titel:</span></label>
-                        <input value="<?php echo $title; ?>" class="form-control" type="text" name="Title" id="title" disabled>
-                    </div>
-                    <div class="form-group">
-                        <label for="categorySelect"><span class="fieldInfo">Kategorie:</span></label><br />
-                        <input value="<?php echo $category; ?>" class="form-control" type="text" name="Category" id="categorySelect" disabled>
-                    </div>
-                    <div class="form-group">
-                        <label for="imageSelect"><span class="fieldInfo">Bild:</span></label><br />
-												<img src="upload/<?php echo $image; ?>" width="170" height="70"/>
-                    </div>
-                    <div class="form-group">
-                        <label for="postArea"><span class="fieldInfo">Artikel:</span></label>
-                        <textarea class="form-control" name="Post" id="postArea" rows="6" cols="50" disabled><?php echo $post; ?></textarea>
-                    </div>
-                    <br>
-                    <input class="btn btn-danger btn-block" type="Submit" name="Submit" value="Artikel löschen">
-                    <br>
-                </fieldset>
-            </form>
-        </div>
-
-    </div> <!-- Main End  -->
-</div>
+    <div class="row">
+        <div class="col-sm-2">
+            &nbsp;
+            <ul id="sidemenu" class="nav nav-pills nav-stacked">
+                <li><a href="dashboard.php">
+                    <span class="glyphicon glyphicon-th"></span>
+                    &nbsp;Dashboard</a>
+                </li>
+                <li  class="active"><a href="addNewPost.php">
+                    <span class="glyphicon glyphicon-list-alt"></span>
+                    &nbsp;Neuer Artikel</a>
+                </li>
+                <li><a href="categories.php">
+                    <span class="glyphicon glyphicon-tags"></span>
+                    &nbsp;Kategorien</a>
+                </li>
+                <li><a href="#">
+                    <span class="glyphicon glyphicon-user"></span>
+                    &nbsp;Manage Admins</a>
+                </li>
+                <li><a href="#">
+                    <span class="glyphicon glyphicon-comment"></span>
+                    &nbsp;Kommentare</a>
+                </li>
+                <li><a href="#">
+                    <span class="glyphicon glyphicon-equalizer"></span>
+                    &nbsp;Live Blog</a>
+                </li>
+                <li><a href="#">
+                    <span class="glyphicon glyphicon-log-out"></span>
+                    &nbsp;Logout</a>
+                </li>
+            </ul>
+        </div> <!-- Sidebar End  -->
+        <div class="col-sm-10">
+            <div>
+                <?php
+                echo message();
+                echo okMessage();
+                ?>
+            </div>
+            <h1>Artikel löschen</h1>
+            <div>
+            <?php
+            $queryId = $_GET["delete"];
+            $connection;
+            $query = "SELECT * FROM admin_panel WHERE id='$queryId'";
+            $execute = mysqli_query($connection, $query);
+            while($dataRows = mysqli_fetch_array($execute)){
+                $title = $dataRows["title"];
+                $category = $dataRows["category"];
+                $image = $dataRows["image"];
+                $post = $dataRows["post"];
+            }
+            ?>
+                <form action="deletePost.php?delete=<?php echo $queryId; ?>" method="post" enctype="multipart/form-data">
+                    <fieldset>
+                        <div class="form-group">
+                            <label for="title"><span class="fieldInfo">Titel:</span></label>
+                            <input value="<?php echo $title; ?>" class="form-control" type="text" name="Title" id="title" disabled>
+                        </div>
+                        <div class="form-group">
+                            <label for="categorySelect"><span class="fieldInfo">Kategorie:</span></label><br />
+                            <input value="<?php echo $category; ?>" class="form-control" type="text" name="Category" id="categorySelect" disabled>
+                        </div>
+                        <div class="form-group">
+                            <label for="imageSelect"><span class="fieldInfo">Bild:</span></label><br />
+                                                    <img src="upload/<?php echo $image; ?>" width="170" height="70"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="postArea"><span class="fieldInfo">Artikel:</span></label>
+                            <textarea class="form-control" name="Post" id="postArea" rows="6" cols="50" disabled><?php echo $post; ?></textarea>
+                        </div>
+                        <br>
+                        <input class="btn btn-danger btn-block" type="Submit" name="Submit" value="Artikel löschen">
+                        <br>
+                    </fieldset>
+                </form>
+            </div>
+        </div> <!-- Main End  -->
+    </div>
 </div>
 <div id="footer">
     <p>&copy;2017 Christian Scheider [chris@devcs.de] --- All rights reserved.</p>
--- editPost.php
+++ editPost.php
@@ -23,27 +23,27 @@ if(isset($_POST["Submit"])){
         $_SESSION["ErrorMessage"] = "Der Titel benötigt mindestens 3 Zeichen.";
         redirect_to("editPost.php");
     } else {
-				global $connection;
-				$editId = $_GET["edit"];
-				if(empty($image)){
-					$query = "UPDATE admin_panel SET datetime='$datetime', title='$title',
-					category='$category', author='$admin', post='$post'
-					WHERE id='$editId'";
-				} else {
-					$query = "UPDATE admin_panel SET datetime='$datetime', title='$title',
-					category='$category', author='$admin', image='$image', post='$post'
-					WHERE id='$editId'";
-				}
-				$execute = mysqli_query($connection, $query);
-				move_uploaded_file($_FILES["Image"]["tmp_name"], $uploadPath);
-				if($execute){
-					$_SESSION["SuccessMessage"] = "Artikel wurde editiert.";
-					redirect_to("dashboard.php");
-				} else {
-					$_SESSION["ErrorMessage"] = "Artikel konnte nicht editiert werden.";
-					redirect_to("editPost.php");
-				}
-		}
+        global $connection;
+        $editId = $_GET["edit"];
+        if(empty($image)){
+            $query = "UPDATE admin_panel SET datetime='$datetime', title='$title',
+            category='$category', author='$admin', post='$post'
+            WHERE id='$editId'";
+        } else {
+            $query = "UPDATE admin_panel SET datetime='$datetime', title='$title',
+            category='$category', author='$admin', image='$image', post='$post'
+            WHERE id='$editId'";
+        }
+        $execute = mysqli_query($connection, $query);
+        move_uploaded_file($_FILES["Image"]["tmp_name"], $uploadPath);
+        if($execute){
+            $_SESSION["SuccessMessage"] = "Artikel wurde editiert.";
+            redirect_to("dashboard.php");
+        } else {
+            $_SESSION["ErrorMessage"] = "Artikel konnte nicht editiert werden.";
+            redirect_to("editPost.php");
+        }
+	}
 }
 
 ?>
@@ -92,72 +92,72 @@ if(isset($_POST["Submit"])){
       </div>
   </nav>
   <div class="cont2"></div>
-<div class="container-fluid">
 
-<div class="row">
-    <div class="col-sm-2">
-          &nbsp;
-        <ul id="sidemenu" class="nav nav-pills nav-stacked">
-            <li><a href="dashboard.php">
-                <span class="glyphicon glyphicon-th"></span>
-                &nbsp;Dashboard</a>
-            </li>
-            <li  class="active"><a href="addNewPost.php">
-                <span class="glyphicon glyphicon-list-alt"></span>
-                &nbsp;Neuer Artikel</a>
-            </li>
-            <li><a href="categories.php">
-                <span class="glyphicon glyphicon-tags"></span>
-                &nbsp;Kategorien</a>
-            </li>
-            <li><a href="#">
-                <span class="glyphicon glyphicon-user"></span>
-                &nbsp;Manage Admins</a>
-            </li>
-            <li><a href="#">
-                <span class="glyphicon glyphicon-comment"></span>
-                &nbsp;Kommentare</a>
-            </li>
-            <li><a href="#">
-                <span class="glyphicon glyphicon-equalizer"></span>
-                &nbsp;Live Blog</a>
-            </li>
-            <li><a href="#">
-                <span class="glyphicon glyphicon-log-out"></span>
-                &nbsp;Logout</a>
-            </li>
-        </ul>
-    </div> <!-- Sidebar End  -->
-    <div class="col-sm-10">
-	      <div>
-	          <?php
-	          echo message();
-	          echo okMessage();
-	          ?>
-	      </div>
-        <h1>Artikel editieren</h1>
-        <div>
-						<?php
-						$queryId = $_GET["edit"];
-						$connection;
-						$query = "SELECT * FROM admin_panel WHERE id='$queryId'";
-						$execute = mysqli_query($connection, $query);
-						while($dataRows = mysqli_fetch_array($execute)){
-							$title = $dataRows["title"];
-	            $category = $dataRows["category"];
-	            $image = $dataRows["image"];
-	            $post = $dataRows["post"];
-						}
-						?>
-            <form action="editPost.php?edit=<?php echo $queryId; ?>" method="post" enctype="multipart/form-data">
-                <fieldset>
-                    <div class="form-group">
-                        <label for="title"><span class="fieldInfo">Titel:</span></label>
-                        <input value="<?php echo $title; ?>" class="form-control" type="text" name="Title" id="title" placeholder="Titel">
-                    </div>
-                    <div class="form-group">
-                        <label for="categorySelect"><span class="fieldInfo">Kategorie:</span></label>
-                        <select class="form-control" id="categorySelect" name="Category">
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-2">
+            &nbsp;
+            <ul id="sidemenu" class="nav nav-pills nav-stacked">
+                <li><a href="dashboard.php">
+                    <span class="glyphicon glyphicon-th"></span>
+                    &nbsp;Dashboard</a>
+                </li>
+                <li  class="active"><a href="addNewPost.php">
+                    <span class="glyphicon glyphicon-list-alt"></span>
+                    &nbsp;Neuer Artikel</a>
+                </li>
+                <li><a href="categories.php">
+                    <span class="glyphicon glyphicon-tags"></span>
+                    &nbsp;Kategorien</a>
+                </li>
+                <li><a href="#">
+                    <span class="glyphicon glyphicon-user"></span>
+                    &nbsp;Manage Admins</a>
+                </li>
+                <li><a href="#">
+                    <span class="glyphicon glyphicon-comment"></span>
+                    &nbsp;Kommentare</a>
+                </li>
+                <li><a href="#">
+                    <span class="glyphicon glyphicon-equalizer"></span>
+                    &nbsp;Live Blog</a>
+                </li>
+                <li><a href="#">
+                    <span class="glyphicon glyphicon-log-out"></span>
+                    &nbsp;Logout</a>
+                </li>
+            </ul>
+        </div> <!-- Sidebar End  -->
+        <div class="col-sm-10">
+            <div>
+                <?php
+                echo message();
+                echo okMessage();
+                ?>
+            </div>
+            <h1>Artikel editieren</h1>
+            <div>
+            <?php
+            $queryId = $_GET["edit"];
+            $connection;
+            $query = "SELECT * FROM admin_panel WHERE id='$queryId'";
+            $execute = mysqli_query($connection, $query);
+            while($dataRows = mysqli_fetch_array($execute)){
+            $title = $dataRows["title"];
+            $category = $dataRows["category"];
+            $image = $dataRows["image"];
+            $post = $dataRows["post"];
+            }
+            ?>
+                <form action="editPost.php?edit=<?php echo $queryId; ?>" method="post" enctype="multipart/form-data">
+                    <fieldset>
+                        <div class="form-group">
+                            <label for="title"><span class="fieldInfo">Titel:</span></label>
+                            <input value="<?php echo $title; ?>" class="form-control" type="text" name="Title" id="title" placeholder="Titel">
+                        </div>
+                        <div class="form-group">
+                            <label for="categorySelect"><span class="fieldInfo">Kategorie:</span></label>
+                            <select class="form-control" id="categorySelect" name="Category">
                             <?php
                             global $connection;
                             $viewquery = "SELECT * FROM category ORDER BY datetime desc";
@@ -166,36 +166,33 @@ if(isset($_POST["Submit"])){
                             while($dataRows = mysqli_fetch_array($execute)){
                                 $id = $dataRows["id"];
                                 $name = $dataRows["name"];
+                                if($name == $category){
                             ?>
-																<?php
-																	if($name == $category){
-																?>
-																	<option selected="selected"><?php echo $name; ?></option>
-														<?php } else { ?>
-                                <option><?php echo $name ?></option>
+                            <option selected="selected"><?php echo $name; ?></option>
+                            <?php } else { ?>
+                            <option><?php echo $name ?></option>
                             <?php
-													  }}
+                            }}
                             ?>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="imageSelect"><span class="fieldInfo">Bild:</span></label><br />
-												<img src="upload/<?php echo $image; ?>" width="170" height="70"/>
-                        <input type="File" class="form-control" name="Image" id="imageSelect" value="<?php echo $image; ?>" >
-                    </div>
-                    <div class="form-group">
-                        <label for="postArea"><span class="fieldInfo">Artikel:</span></label>
-                        <textarea class="form-control" name="Post" id="postArea" rows="6" cols="50"><?php echo $post; ?></textarea>
-                    </div>
-                    <br>
-                    <input class="btn btn-info btn-block" type="Submit" name="Submit" value="Artikel editieren">
-                    <br>
-                </fieldset>
-            </form>
-        </div>
-
-    </div> <!-- Main End  -->
-</div>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="imageSelect"><span class="fieldInfo">Bild:</span></label><br />
+                                                    <img src="upload/<?php echo $image; ?>" width="170" height="70"/>
+                            <input type="File" class="form-control" name="Image" id="imageSelect" value="<?php echo $image; ?>" >
+                        </div>
+                        <div class="form-group">
+                            <label for="postArea"><span class="fieldInfo">Artikel:</span></label>
+                            <textarea class="form-control" name="Post" id="postArea" rows="6" cols="50"><?php echo $post; ?></textarea>
+                        </div>
+                        <br>
+                        <input class="btn btn-info btn-block" type="Submit" name="Submit" value="Artikel editieren">
+                        <br>
+                    </fieldset>
+                </form>
+            </div>
+        </div> <!-- Main End  -->
+    </div>
 </div>
 <div id="footer">
     <p>&copy;2017 Christian Scheider [chris@devcs.de] --- All rights reserved.</p>
